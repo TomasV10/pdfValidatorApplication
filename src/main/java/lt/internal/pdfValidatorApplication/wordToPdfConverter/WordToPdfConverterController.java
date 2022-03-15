d95a0a56a36129b37630e05e78fa5284d16f6d7d
TomasV10 <tomasvarnauskas19@gmail.com>
2022-03-15 18:19:32 +0200

    Multiple file upload at once functionality added.

--- src/main/java/lt/internal/pdfValidatorApplication/wordToPdfConverter/WordToPdfConverterController.java
+++ src/main/java/lt/internal/pdfValidatorApplication/wordToPdfConverter/WordToPdfConverterController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,33 +24,22 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class WordToPdfConverterController {
 
     private final WordToPdfConverterService wordToPdfConverterService;
-    private String message = "";
 
     public WordToPdfConverterController(WordToPdfConverterService wordToPdfConverterService) {
         this.wordToPdfConverterService = wordToPdfConverterService;
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<UploadMessage>uploadFile(@RequestParam("file") MultipartFile file){
-        if(wordToPdfConverterService.hasDocOrDocxFormat(file)) {
-            try {
-                wordToPdfConverterService.saveFiles(file);
-                message = "Uploaded the file successfully";
-                return ResponseEntity.status(OK).body(new UploadMessage(file.getOriginalFilename(),
-                        Collections.singletonList(message), true));
-            } catch (Exception e) {
-                message = "Could not upload the file";
-                return ResponseEntity.status(EXPECTATION_FAILED)
-                            .body(new UploadMessage(file.getOriginalFilename(),
-                                    Collections.singletonList(message), false));
-            }
-        }
-        message = "Please upload a .doc or .docx file!";
-        return ResponseEntity.status(BAD_REQUEST).body(createUploadMessage(file.getOriginalFilename(), message,
-                                                                                                    false));
+    @PostMapping("/uploadFiles")
+    public ResponseEntity<List<UploadMessage>> saveDocFile(@RequestParam("files") MultipartFile[] files){
+        List<UploadMessage>messages =
+                 Arrays.stream(files)
+                    .map(this::getUploadMessage)
+                    .collect(Collectors.toList());
+        return ResponseEntity.status(EXPECTATION_FAILED)
+                            .body(messages);
     }
 
-    @PostMapping(path = {"/files/convert","/files/convert/{fileName}"})
+    @PostMapping(path = {"/files/convert"})
     public ResponseEntity<List<ResponseMessage>> convertAllFiles(@RequestParam(value = "fileName",required = false )
                                                                              String fileName){
         List<ResponseMessage> responseMessages = createResponseMessages(fileName);
@@ -60,11 +50,11 @@ public class WordToPdfConverterController {
     @DeleteMapping("/files/delete/{fileName}")
     public ResponseEntity<DeleteMessage>deleteFile(@PathVariable("fileName") String fileName) throws IOException {
             if(wordToPdfConverterService.deleteFileIfExist(fileName)) {
-                message = "Deleted file successfully";
+                String message = "Deleted file successfully";
                 return ResponseEntity.status(OK).body(new DeleteMessage(fileName,
                         Collections.singletonList(message), true));
             }else {
-                message = "File not found";
+                String message = "File not found";
                 return ResponseEntity.status(BAD_REQUEST).body(new DeleteMessage(fileName,
                     Collections.singletonList(message), false));
             }
@@ -95,7 +85,7 @@ public class WordToPdfConverterController {
 
     private List<ResponseMessage> createResponseMessages(String fileName) {
         List<PdfMessages>pdfMessages = WordToPdfConverterService.convertAllDocFilesInDirectoryToPdf(ofNullable(fileName));
-        List<ResponseMessage>responseMessages = pdfMessages.stream()
+        return pdfMessages.stream()
                 .map(er -> {
                     String nameOfFile = er.getFileName();
                     List<String>msg = er.getMessages();
@@ -105,7 +95,26 @@ public class WordToPdfConverterController {
                     return new ResponseMessage(nameOfFile, msg, isValid, isConvertedToPdf, url);
                 })
                 .collect(Collectors.toList());
-        return responseMessages;
     }
 
+     /*
+    Creates response message with upload results
+     */
+
+    private UploadMessage getUploadMessage(MultipartFile file) {
+        if(wordToPdfConverterService.hasDocOrDocxFormat(file)) {
+            try {
+                wordToPdfConverterService.saveFiles(file);
+                String message = "Uploaded the file successfully";
+                return new UploadMessage(file.getOriginalFilename(),
+                        Collections.singletonList(message), true);
+            } catch (Exception e) {
+                String message = "Could not upload the file";
+                return new UploadMessage(file.getOriginalFilename(),
+                        Collections.singletonList(message), false);
+            }
+        }
+        String message = "Please upload a .doc or .docx file!";
+        return createUploadMessage(file.getOriginalFilename(), message, false);
+    }
 }
